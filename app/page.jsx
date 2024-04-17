852bbff79df6c15db88bdee0cedd4ff94d29b5c0
Mekontso <mekontso.neoworlder@gmail.com>
2024-04-17 09:57:17 +0100

    Add prettier code format

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <section className="py-24">
       <div className="container">
-        <h1 className="text-3xl font-bold">NextJS 14 starter kit</h1>
+        <h1 className="mt-4 text-3xl font-bold">NextJS 14 starter kit</h1>
       </div>
     </section>
   );
